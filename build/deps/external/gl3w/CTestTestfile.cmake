20bdef69831b2a9b07c45491a54b4a2cb2232281
Alexey Lobanov <zeromarblectm@gmail.com>
2022-09-01 02:08:19 +0000

    qweq

--- build/deps/external/gl3w/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/programming/c++/coding4-jenny/hw1/deps/canvas/deps/external/gl3w
-# Build directory: /home/alex/programming/c++/coding4-jenny/hw1/deps/canvas/build/deps/external/gl3w
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/deps/external/glfw/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/programming/c++/coding4-jenny/hw1/deps/canvas/deps/external/glfw
-# Build directory: /home/alex/programming/c++/coding4-jenny/hw1/deps/canvas/build/deps/external/glfw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
